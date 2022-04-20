7bb7e3dd470572662fecac1bd5a7312a82c734a6
xuliangpan <xuliangpan@xeg.com>
2022-04-20 21:45:48 +0800

    delegate mode

---
+++ src/main/java/com/xulp/delegate/simple/EmployeeB.java
@@ -0,0 +1,15 @@
+package com.xulp.delegate.simple;
+
+import java.text.MessageFormat;
+
+/**
+ * @Description TODO
+ * @Author xulp
+ * @Date 2022/4/20 20:05
+ */
+public class EmployeeB implements IEmployee {
+    @Override
+    public void doing(String command) {
+        System.out.println(MessageFormat.format("我是员工B，我开始干活了，擅长{0}，执行{1}...", command, command));
+    }
+}
